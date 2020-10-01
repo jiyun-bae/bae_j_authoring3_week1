b7a13ece36aeb0b60b76314b123ef516212c0d80
Jiyun <baejiyun01@gmail.com>
2020-10-02 04:06:19 +0900

    Uploading Everything

--- js/modules/DataModule.js
+++ js/modules/DataModule.js
@@ -1,5 +1,25 @@
 export default {
-    name: "Bae",
-    role: "Student",
-    nickname: "Bae"
+    Bae: {
+        name: "Bae",
+        role: "Student",
+        nickname: "Bae"
+    },
+
+    John: {
+        name: "John",
+        role: "super chill prof",
+        nickname: "jsut john"
+    },
+
+    Joe: {
+        name: "Joe",
+        role: "Prof",
+        nickname: "Teddy Bear"
+    },
+
+    Pan: {
+        name: "Pan",
+        role: "Code Ninja",
+        nickname: "Spider Pan"
+    }
 }
